f672befc3fe59e25423e69c7af0fb1a2840614be
Irina Ageenko <ira.ageenkoo@gmail.com>
2019-04-30 13:02:24 +0300

    added empty page component; fixed buttons on dashboard page

--- client/app/_dashboards/dashboard.module.js
+++ client/app/_dashboards/dashboard.module.js
@@ -20,6 +20,8 @@ require('echarts/lib/component/calendar');
 //TODO: fix DI and use as custom module
 require('../../vendors/ngecharts');
 
+import emptyPageComponent from '../shared/empty-page/empty-page.component';
+
 import ScreenshotService from './screenshot.util';
 import dashboardComponent from './dashboard.component';
 
@@ -30,4 +32,5 @@ export const dashboardModule = angular.module('app.dashboard', [
     'ngecharts',
     ])
     .component({ dashboardComponent })
+    .component({ emptyPageComponent })
     .service('$screenshot', ScreenshotService);
