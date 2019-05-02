0749ae31a88b90f5513638067b815414336028f7
Irina Ageenko <ira.ageenkoo@gmail.com>
2019-05-02 10:10:56 +0300

    added empty page component ro users page

--- client/app/_users/users.module.js
+++ client/app/_users/users.module.js
@@ -1,4 +1,6 @@
 import usersComponent from './users.component';
+import emptyPageComponent from '../shared/empty-page/empty-page.component';
 
 export const usersModule = angular.module('app.users', [])
+    .component({ emptyPageComponent })
     .component({ usersComponent });
