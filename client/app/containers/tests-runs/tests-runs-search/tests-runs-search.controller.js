8477974213e817ad925eba944ce7a0beaa6f9947
zmutnik <amweb@tuta.io>
2020-01-08 15:58:51 +0300

    Fixed date formating in filter

--- client/app/containers/tests-runs/tests-runs-search/tests-runs-search.controller.js
+++ client/app/containers/tests-runs/tests-runs-search/tests-runs-search.controller.js
@@ -58,7 +58,7 @@ const TestsRunsSearchController = function TestsRunsSearchController(windowWidth
         $mdDialog.cancel();
     };
 
-    
+
     function showSearchDialog(event) {
         $mdDialog.show({
             controller: SearchModalController,
@@ -69,7 +69,7 @@ const TestsRunsSearchController = function TestsRunsSearchController(windowWidth
             controllerAs: '$ctrl',
             bindToController: true,
             onComplete: () => {
-                $(window).on('resize.searchDialog',() => { 
+                $(window).on('resize.searchDialog',() => {
                     if ($(window).width() >= mobileWidth) {
                         vm.closeModal();
                     }
@@ -92,7 +92,7 @@ const TestsRunsSearchController = function TestsRunsSearchController(windowWidth
         if (vm.isSearchActive()) {
             let fromDate = testsRunsService.getSearchParam('fromDate');
             let toDate = testsRunsService.getSearchParam('toDate');
-            const date = testsRunsService.getSearchParam('date'); 
+            const date = testsRunsService.getSearchParam('date');
             const selectedTemplateName = testsRunsService.getSearchParam('selectedTemplateName');
 
             date && (fromDate = toDate = date);
@@ -236,11 +236,24 @@ const TestsRunsSearchController = function TestsRunsSearchController(windowWidth
         .then(function(result) {
             if (result) {
                 vm.selectedRange = result;
-                vm.selectedRange.selectedTemplateName = result.selectedTemplateName.split(' ').slice(0,-1).join(' ');
-                vm.searchParams.selectedTemplateName = vm.selectedRange.selectedTemplateName;
+
+                if(result.selectedTemplate) {
+                    if (vm.selectedRange.dateStart.getMonth() === vm.selectedRange.dateEnd.getMonth()) {
+                        if(vm.selectedRange.dateStart.getTime() === vm.selectedRange.dateEnd.getTime()) {
+                            vm.selectedRange.selectedTemplateName =  moment(vm.selectedRange.dateStart).format('DD') + ' ' + moment(vm.selectedRange.dateStart).format('MMM');
+                        } else {
+                            vm.selectedRange.selectedTemplateName = moment(vm.selectedRange.dateStart).format('DD') + ' - ' + moment(vm.selectedRange.dateEnd).format('DD') + ' ' + moment(vm.selectedRange.dateStart).format('MMM');
+                        }
+                    } else {
+                        vm.selectedRange.selectedTemplateName = moment(vm.selectedRange.dateStart).format('DD') + ' ' + moment(vm.selectedRange.dateStart).format('MMM') + ' - ' + moment(vm.selectedRange.dateEnd).format('DD') + ' ' + moment(vm.selectedRange.dateEnd).format('MMM');
+                    }
+                } else {
+                    vm.selectedRange.selectedTemplateName = result.selectedTemplateName.split(' ').slice(0,-1).join(' ');
+                    vm.searchParams.selectedTemplateName = vm.selectedRange.selectedTemplateName;
+                }
+
                 if (vm.selectedRange.dateStart && vm.selectedRange.dateEnd) {
-                    if (vm.selectedRange.dateStart.getTime() !==
-                        vm.selectedRange.dateEnd.getTime()) {
+                    if (vm.selectedRange.dateStart.getTime() !== vm.selectedRange.dateEnd.getTime()) {
                         vm.searchParams.date = null;
                         vm.searchParams.fromDate = vm.selectedRange.dateStart;
                         vm.searchParams.toDate = vm.selectedRange.dateEnd;
