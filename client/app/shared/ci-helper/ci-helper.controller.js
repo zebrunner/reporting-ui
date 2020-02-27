0ae4843c630275347980dce173e3896997edc12e
Victor Mitskevich <vmitskevich@solvd.com>
2020-02-27 12:02:01 +0300

    refactor get type function

--- client/app/shared/ci-helper/ci-helper.controller.js
+++ client/app/shared/ci-helper/ci-helper.controller.js
@@ -246,16 +246,24 @@ const CiHelperController = function CiHelperController(
         });
     }
 
+    function isNumeric(value) {
+        return !isNaN(value - parseFloat(value));
+    }
+
+    function isString(value) {
+        return typeof value === 'string' || value instanceof String;
+    }
+
     $scope.getType = function (value) {
-        return angular.isArray(value)
-            ? 'array'
-            : typeof value === "boolean"
-            ? 'boolean'
-            : !isNaN(+value) && !isNaN(parseInt(value, 10))
-            ? 'int'
-            : typeof value === 'string' || value instanceof String
-            ? 'string'
-            : 'none';
+        if (angular.isArray(value)) {
+            return 'array';
+        } else if (typeof value === 'boolean') {
+            return 'boolean';
+        } else if (isNumeric(value)) {
+            return 'int';
+        } else if (isString(value)) {
+            return 'string';
+        } else return 'none';
     };
 
     $scope.getElement = function (item) {
