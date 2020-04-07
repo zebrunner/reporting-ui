f0f5702ff9a3bf55bf8e751077c9457a76dec358
Victor Mitskevich <vmitskevich@solvd.com>
2020-04-07 16:26:17 +0300

    ZEB-998/ fix selected filter icon

--- client/app/containers/test-run-info/test-run-info.module.js
+++ client/app/containers/test-run-info/test-run-info.module.js
@@ -1,6 +1,13 @@
 import testRunInfoComponent from './test-run-info.component';
 import elasticsearchService from './elasticsearch.service';
+import checkedListIcon from '../../../assets/images/check_list.svg';
 
 export const testRunInfoModule = angular.module('app.testRunInfo', [])
     .factory({ elasticsearchService })
-    .component({ testRunInfoComponent });
+    .component({ testRunInfoComponent })
+    .config(function ($mdIconProvider) {
+        'ngInject';
+        
+        $mdIconProvider
+            .icon('artifacts:checkedList', checkedListIcon);
+    });
