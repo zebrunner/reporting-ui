420d8dd19bb74b7b46e0fd0f885be4933e67819f
DmitryMishin <dmishin@solvd.com>
2020-05-06 12:14:17 +0300

    export react component/update package.json with peerDeps

--- reactify/containers/signin/index.js
+++ reactify/containers/signin/index.js
@@ -1,1 +1,5 @@
+import { reactify } from '@zebrunner/core';
+
 export * from './signin.module';
+
+export default reactify('<signin-component />');
--- reactify/containers/signin/signin.module.js
+++ reactify/containers/signin/signin.module.js
@@ -1,4 +1,6 @@
 import angular from 'angular';
+import { RouterModule } from '@zebrunner/core';
+
 import { signinComponent } from './signin.component';
 import { CopyrightModule } from '../../shared/copyright';
 import { BackgroundModule } from './shared/background';
@@ -6,6 +8,7 @@ import { BackgroundModule } from './shared/background';
 export const SigninModule = angular.module('zebrunner.signin', [
     CopyrightModule,
     BackgroundModule,
+    RouterModule,
 ])
 
     .component({ signinComponent })
