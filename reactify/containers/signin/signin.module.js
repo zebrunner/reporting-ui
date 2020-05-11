0ad0ec6efd2aa2b4cc973018e9c1fec34835e29e
DmitryMishin <dmishin@solvd.com>
2020-05-11 23:44:27 +0300

    remove router module from deps

--- reactify/containers/signin/signin.module.js
+++ reactify/containers/signin/signin.module.js
@@ -1,5 +1,4 @@
 import angular from 'angular';
-import { RouterModule } from '@zebrunner/core';
 
 import { signinComponent } from './signin.component';
 import { CopyrightModule } from '../../shared/copyright';
@@ -8,7 +7,6 @@ import { BackgroundModule } from './shared/background';
 export const SigninModule = angular.module('zebrunner.signin', [
     CopyrightModule,
     BackgroundModule,
-    RouterModule,
 ])
 
     .component({ signinComponent })
