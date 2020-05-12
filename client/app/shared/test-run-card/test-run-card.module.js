8bfc0dbbd31aa50f8d1694ecaa205a07c37d3737
Victor Mitskevich <vmitskevich@solvd.com>
2020-05-12 21:05:55 +0300

    ZEB-1267/ fix icons

--- client/app/shared/test-run-card/test-run-card.module.js
+++ client/app/shared/test-run-card/test-run-card.module.js
@@ -1,9 +1,20 @@
 'use strict';
 
 import testRunCard from './test-run-card.directive';
+import blockerIcon from '../../../assets/images/_icons_test-run-card/icon_block.svg';
+import reviewedIcon from '../../../assets/images/_icons_test-run-card/icon_reviewed.svg';
+import commentIcon from '../../../assets/images/_icons_test-run-card/icon_comment.svg';
 
 import './test-run-card.scss'
 
 export const testRunCardModule = angular.module('testRunCard', [])
     .directive({ testRunCard })
+    .config(function ($mdIconProvider) {
+        'ngInject';
+        
+        $mdIconProvider
+        .icon('blocker', blockerIcon)
+        .icon('reviewed', reviewedIcon)
+        .icon('comment', commentIcon);
+    })
     .name;
