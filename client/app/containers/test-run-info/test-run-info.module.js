e47fc01b1eb8f266dcc5a58c8aebc8315c0ce81d
Daniel Burkov <dburkov@solvd.com>
2020-05-22 10:15:53 +0300

    replaced no-video icon and fixed texts

--- client/app/containers/test-run-info/test-run-info.module.js
+++ client/app/containers/test-run-info/test-run-info.module.js
@@ -1,13 +1,17 @@
+'use strict';
+
 import testRunInfoComponent from './test-run-info.component';
 import elasticsearchService from './elasticsearch.service';
 import checkedListIcon from '../../../assets/images/check_list.svg';
+import videocam_off_outlined from '../../../assets/images/videocam_off_outlined.svg';
 
 export const testRunInfoModule = angular.module('app.testRunInfo', [])
     .factory({ elasticsearchService })
     .component({ testRunInfoComponent })
-    .config(function ($mdIconProvider) {
+    .config(($mdIconProvider) => {
         'ngInject';
-        
+
         $mdIconProvider
-            .icon('checkedListIcon', checkedListIcon);
+            .icon('checkedListIcon', checkedListIcon)
+            .icon('outlined:videocam_off', videocam_off_outlined);
     });
