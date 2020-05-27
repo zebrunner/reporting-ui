a526b9e979c34e420cd07cc719f3c359ebce3fed
DmitryMishin <dmishin@solvd.com>
2020-05-27 13:34:39 +0300

    remove unused controllers

--- client/app/migration/shared/auth/shared/copyright/copyright.component.js
+++ client/app/migration/shared/auth/shared/copyright/copyright.component.js
@@ -1,9 +1,7 @@
 import template from './copyright.component.html';
-import controller from './copyright.controller';
 
 export const copyrightComponent = {
     template,
-    controller,
     bindings: {
         versions: '<',
     },
