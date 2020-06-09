3bc1b09616a6c9fafbad47b5c1a748e5dd8e21b0
Victor Mitskevich <vmitskevich@solvd.com>
2020-06-09 15:37:16 +0300

    ZEB-1265/ minor fixes

--- client/app/_users/users.module.js
+++ client/app/_users/users.module.js
@@ -1,4 +1,11 @@
 import usersComponent from './users.component';
+import checkedListIcon from '../../assets/images/check_list.svg';
 
 export const usersModule = angular.module('app.users', [])
-    .component({ usersComponent });
+    .component({ usersComponent })
+    .config(($mdIconProvider) => {
+        'ngInject';
+
+        $mdIconProvider
+            .icon('checkedListIcon', checkedListIcon)
+    });
