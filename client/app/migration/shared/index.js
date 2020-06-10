7cd372a46ddb2117964abf2780445df2b41adc6e
DmitryMishin <dmishin@solvd.com>
2020-06-10 16:24:43 +0300

    remove prod issue

--- client/app/migration/shared/index.js
+++ client/app/migration/shared/index.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 
 import { AuthModule } from './auth/auth.module';
 
-export const MigrationSharedModule = angular.module('zebrunner.reporting.migration.shared', [
+export const ReportingMigrationSharedModule = angular.module('zebrunner.reporting.migration.shared', [
     AuthModule,
 ])
 
