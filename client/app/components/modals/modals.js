57f74db6e315a84ac6ce723730cdf5274b19d2ca
Daniel Burkov <dburkov@solvd.com>
2020-06-22 09:43:00 +0300

    removed unused modal

--- client/app/components/modals/modals.js
+++ client/app/components/modals/modals.js
@@ -4,5 +4,4 @@ require('./compare/compare.controller');
 require('./email/email.controller');
 require('./gallery/gallery.controller');
 require('./rerun/rerun.controller');
-require('./test-info/test-info.controller');
 require('./project/project.controller');
