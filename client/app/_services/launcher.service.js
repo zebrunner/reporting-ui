1a1c45d230fe95996fcfb50f9839caff1afb481b
Daniel Burkov <dburkov@solvd.com>
2020-07-14 21:19:23 +0300

    minor fixes

--- client/app/_services/launcher.service.js
+++ client/app/_services/launcher.service.js
@@ -5,9 +5,9 @@
         .module('app.services')
         .factory('LauncherService', ['$httpMock', '$rootScope', 'UtilService', LauncherService])
 
-    function LauncherService($http, $rootScope, UtilService) {
+    function LauncherService($httpMock, $rootScope, UtilService) {
 
-        const service = {
+        return {
             abortScanRepository,
             buildLauncher,
             createLauncher,
@@ -25,80 +25,78 @@
             updateLauncherConfig,
         };
 
-        return service;
-
         function createLauncher(launcher, automationServerId) {
-            return $http.post(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers?automationServerId=${automationServerId}`, launcher)
+            return $httpMock.post(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers?automationServerId=${automationServerId}`, launcher)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to create launcher'));
         }
 
         function saveLauncherConfig(launcherId, params) {
-            return $http.post(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${launcherId}/presets`, params)
+            return $httpMock.post(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${launcherId}/presets`, params)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to save launcher config'));
         }
 
         function updateLauncherConfig(laucnherId, configId, params) {
-            return $http.put(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${laucnherId}/presets/${configId}`, params)
+            return $httpMock.put(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${laucnherId}/presets/${configId}`, params)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to update launcher config'));
         }
 
         function deleteLauncherConfig(laucnherId, configId) {
-            return $http.delete(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${laucnherId}/presets/${configId}`)
+            return $httpMock.delete(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${laucnherId}/presets/${configId}`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to delete launcher config'));
         }
 
         function getConfigHook(laucnherId, configId) {
-            return $http.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${laucnherId}/presets/${configId}/hook`)
+            return $httpMock.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${laucnherId}/presets/${configId}/hook`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to get webhook'));
         }
 
         function getLauncherById(id) {
-            return $http.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${id}`)
+            return $httpMock.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${id}`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to get launcher'));
         }
 
         function getAllLaunchers() {
-            return $http.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers`)
+            return $httpMock.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to get all launchers'));
         }
 
         function updateLauncher(launcher) {
-            return $http.put(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers`, launcher)
+            return $httpMock.put(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers`, launcher)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to update launcher'));
         }
 
         function deleteLauncherById(id) {
-            return $http.delete(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${id}`)
+            return $httpMock.delete(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${id}`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to delete launcher'));
         }
 
         function buildLauncher(launcher, providerId) {
-            return $http.post(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/build${providerId ? `?providerId=${providerId}` : ''}`, launcher)
+            return $httpMock.post(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/build${providerId ? `?providerId=${providerId}` : ''}`, launcher)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to build with launcher'));
         }
 
         function setFavouriteLauncher(id, params) {
-            return $http.patch(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${id}`, params)
+            return $httpMock.patch(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/${id}`, params)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to update launcher parameter'));
         }
 
         function scanRepository(launcherScanner, automationServerId) {
-            return $http.post(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/scanner?automationServerId=${automationServerId}`, launcherScanner)
+            return $httpMock.post(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/scanner?automationServerId=${automationServerId}`, launcherScanner)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to scan repository'));
         }
 
         function getBuildNumber(queueItemUrl) {
-            return $http.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/build/number?queueItemUrl=${queueItemUrl}`)
+            return $httpMock.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/build/number?queueItemUrl=${queueItemUrl}`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to get build number'));
         }
 
         function abortScanRepository(buildNumber, scmAccountId, rescan) {
-            return $http.delete(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/scanner/${buildNumber}?scmAccountId=${scmAccountId}&rescan=${rescan}`)
+            return $httpMock.delete(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/scanner/${buildNumber}?scmAccountId=${scmAccountId}&rescan=${rescan}`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to cancel repository scanning'));
         }
 
         function isScannerInProgress(buildNumber, scmAccountId, rescan) {
-            return $http.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/scanner/${buildNumber}/status?scmAccountId=${scmAccountId}&rescan=${rescan}`)
+            return $httpMock.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/launchers/scanner/${buildNumber}/status?scmAccountId=${scmAccountId}&rescan=${rescan}`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to check repository scanning state'));
         }
     }
