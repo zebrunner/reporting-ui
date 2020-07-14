3ca9f76e7075282d90c41ab691dcc0de1903434f
Daniel Burkov <dburkov@solvd.com>
2020-07-14 21:19:23 +0300

    minor fixes

--- client/app/_services/scm.service.js
+++ client/app/_services/scm.service.js
@@ -3,9 +3,9 @@
 
     angular
         .module('app.services')
-        .factory('ScmService', ['$http', '$location', 'UtilService', ScmService])
+        .factory('ScmService', ['$httpMock', '$location', 'UtilService', ScmService])
 
-    function ScmService($http, $location, UtilService) {
+    function ScmService($httpMock, $location, UtilService) {
 
         return {
             invitations: [],
@@ -20,42 +20,42 @@
         };
 
         function updateScmAccount(scmAccount) {
-            return $http.put(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/accounts`, scmAccount)
+            return $httpMock.put(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/accounts`, scmAccount)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to create scm account'));
         }
 
         function getGithubConfig() {
-            return $http.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/github/config`)
+            return $httpMock.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/github/config`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to get Github config'));
         }
 
         function getAllScmAccounts() {
-            return $http.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/accounts`)
+            return $httpMock.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/accounts`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to get list of scm accounts'));
         }
 
         function getDefaultBranch(id) {
-            return $http.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/accounts/${id}/defaultBranch`)
+            return $httpMock.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/accounts/${id}/defaultBranch`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to get default branch'));
         }
 
         function deleteScmAccount(id) {
-            return $http.delete(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/accounts/${id}`)
+            return $httpMock.delete(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/accounts/${id}`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to delete repository'));
         }
 
         function getRepositories(id, org) {
-            return $http.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/github/repositories/${id}?org=${org}`)
+            return $httpMock.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/github/repositories/${id}?org=${org}`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to get client id'));
         }
 
         function getOrganizations(id) {
-            return $http.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/github/organizations/${id}`)
+            return $httpMock.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/github/organizations/${id}`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to get client id'));
         }
 
         function exchangeCode(code) {
-            return $http.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/github/exchange?code=${code}`)
+            return $httpMock.get(`${$httpMock.apiHost}${$httpMock.reportingPath}/api/scm/github/exchange?code=${code}`)
                 .then(UtilService.handleSuccess, UtilService.handleError('Unable to get client id'));
         }
     }
